789aac4c86aeef3d463b0b67b808041efccefd1e
Zehra Aykan <139782347+aykan001@users.noreply.github.com>
2025-01-26 15:01:52 +0300

    Add files via upload

---
+++ JavaScript/educational_series/Application.js
@@ -0,0 +1,15 @@
+var marka=prompt("Marka giriniz: ");
+console.log(marka);
+
+var model=prompt("Model giriniz: ");
+console.log(marka+model);
+
+var maxhiz=prompt("Maxsimum hız: ");
+
+var yakitdeposu=prompt("Yakit deposu kaç litre: ");
+console.log(yakitdeposu*5.60);
+
+var otomatik=prompt("otomatik mi: ");
+console.log(otomatik);
+
+console.log(marka+' '+model+' '+yakitdeposu+' litre yakıt deposuna sahip ve maksimum '+maxhiz+' km hız yapmaktadır.'+ 'Peki araç otomatik mi ? '+otomatik);
